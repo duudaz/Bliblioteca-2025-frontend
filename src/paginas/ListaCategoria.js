4295dce48f2d985603d396580b55404bef240eaf
duudaz <me.rodrigues2205@gmail.com>
2025-05-13 22:00:06 -0300

    Criação das paginas padrões - aula 13/05

--- src/paginas/ListaCategoria.js
+++ src/paginas/ListaCategoria.js
@@ -7,6 +7,51 @@ export default function ListaCategoria() {
         descricao= "Gerencie aqui as categorias dos livros da biblioteca"
         rota= "/ListaCategoria" />
 
+        <div className="table-responsive">
+  <table className="table table-dark table-hover text-center align-middle">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Primeiro Nome</th>
+        <th>Sobrenome</th>
+        <th>Usuário</th>
+        <th>Ações</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td>
+          <button className="btn btn-sm btn-secondary me-1">Editar</button>
+          <button className="btn btn-sm btn-danger">Excluir</button>
+        </td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        <td>
+          <button className="btn btn-sm btn-secondary me-1">Editar</button>
+          <button className="btn btn-sm btn-danger">Excluir</button>
+        </td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Larry</td>
+        <td>Bird</td>
+        <td>@twitter</td>
+        <td>
+          <button className="btn btn-sm btn-secondary me-1">Editar</button>
+          <button className="btn btn-sm btn-danger">Excluir</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
         </>
     );
 };
